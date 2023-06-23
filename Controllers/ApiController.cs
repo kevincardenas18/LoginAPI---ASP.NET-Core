4ab55db6bc7af3170524f3b5d2c3526f1369f136
Kevin Cárdenas <kevin.cardenas-1@outlook.com>
2023-06-23 02:00:28 -0500

    Update Trabajadores

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -363,11 +363,229 @@ namespace LoginAPI___ASP.NET_Core.Controllers
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
-                return Content(content, "application/json");
+                return Ok(content);
             }
             else
             {
                 return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/delete")]
+        public async Task<ActionResult> DeleteTrabajadorAsync(int sucursal, string codigoempleado)
+        {
+
+            var url = $"/api/Varios/TrabajadorDelete?sucursal={sucursal}&codigoempleado={codigoempleado}";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetTipoTrabajador")]
+        public async Task<ActionResult> GetTipoTrabajadorAsync()
+        {
+            var url = "/api/Varios/TipoTrabajador";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetGenero")]
+        public async Task<ActionResult> GetGeneroAsync()
+        {
+            var url = "/api/Varios/Genero";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetEstadoTrabajador")]
+        public async Task<ActionResult> GetEstadoTrabajadorAsync()
+        {
+            var url = "/api/Varios/EstadoTrabajador";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetTipoContrato")]
+        public async Task<ActionResult> GetTipoContratoAsync()
+        {
+            var url = "/api/Varios/TipoContrato";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetTipoCese")]
+        public async Task<ActionResult> GetTipoCeseAsync()
+        {
+            var url = "/api/Varios/TipoCese";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetEstadoCivil")]
+        public async Task<ActionResult> GetEstadoCivilAsync()
+        {
+            var url = "/api/Varios/EstadoCivil";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetEsReingreso")]
+        public async Task<ActionResult> GetEsReingresoAsync()
+        {
+            var url = "/api/Varios/EsReingreso";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("trabajador/GetTipoCuenta")]
+        public async Task<ActionResult> GetTipoCuentaAsync()
+        {
+            var url = "/api/Varios/TipoCuenta";
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("trabajador/Insert")]
+        public async Task<ActionResult> InsertarTrabajadorAsync(int COMP_Codigo, string Tipo_trabajador, string Apellido_Paterno, string Apellido_Materno, string Nombres, string Identificacion,
+            string Entidad_Bancaria, string CarnetIESS, string Direccion, string Telefono_Fijo, string Telefono_Movil, string Genero, string Nro_Cuenta_Bancaria, string Codigo_Categoria_Ocupacion,
+            string Ocupacion, string Centro_Costos, string Nivel_Salarial, string EstadoTrabajador, string Tipo_Contrato, string Tipo_Cese, string EstadoCivil, string TipodeComision, DateTime FechaNacimiento,
+            DateTime FechaIngreso, DateTime FechaCese, int PeriododeVacaciones, DateTime FechaReingreso, DateTime Fecha_Ult_Actualizacion, string EsReingreso, int BancoCTA_CTE, string Tipo_Cuenta, int RSV_Indem_Acumul,
+            int Año_Ult_Rsva_Indemni, int Mes_Ult_Rsva_Indemni, int FormaCalculo13ro, int FormaCalculo14ro, int BoniComplementaria, int BoniEspecial, int Remuneracion_Minima, int CuotaCuentaCorriente,
+            string Fondo_Reserva)
+        {
+
+
+
+            var url = $"/api/Varios/TrabajadorInsert?COMP_Codigo={COMP_Codigo}&Tipo_trabajador={Tipo_trabajador}&Apellido_Paterno={Apellido_Paterno}&Apellido_Materno={Apellido_Materno}&Nombres={Nombres}&Identificacion={Identificacion}&Entidad_Bancaria={Entidad_Bancaria}&CarnetIESS={CarnetIESS}&Direccion={Direccion}&Telefono_Fijo={Telefono_Fijo}&Telefono_Movil={Telefono_Movil}&Genero={Genero}&Nro_Cuenta_Bancaria={Nro_Cuenta_Bancaria}&Codigo_Categoria_Ocupacion={Codigo_Categoria_Ocupacion}&Ocupacion={Ocupacion}&Centro_Costos={Centro_Costos}&Nivel_Salarial={Nivel_Salarial}&EstadoTrabajador={EstadoTrabajador}&Tipo_Contrato={Tipo_Contrato}&Tipo_Cese={Tipo_Cese}&EstadoCivil={EstadoCivil}&TipodeComision={TipodeComision}&FechaNacimiento={FechaNacimiento}&FechaIngreso={FechaIngreso}&FechaCese={FechaCese}&PeriododeVacaciones={PeriododeVacaciones}&FechaReingreso={FechaReingreso}&Fecha_Ult_Actualizacion={Fecha_Ult_Actualizacion}&EsReingreso={EsReingreso}&BancoCTA_CTE={BancoCTA_CTE}&Tipo_Cuenta={Tipo_Cuenta}&RSV_Indem_Acumul={RSV_Indem_Acumul}&Año_Ult_Rsva_Indemni={Año_Ult_Rsva_Indemni}&Mes_Ult_Rsva_Indemni={Mes_Ult_Rsva_Indemni}&FormaCalculo13ro={FormaCalculo13ro}&FormaCalculo14ro={FormaCalculo14ro}&BoniComplementaria={BoniComplementaria}&BoniEspecial={BoniEspecial}&Remuneracion_Minima={Remuneracion_Minima}&CuotaCuentaCorriente={CuotaCuentaCorriente}&Fondo_Reserva={Fondo_Reserva}";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("trabajador/Edit")]
+        public async Task<ActionResult> EditarTrabajadorAsync(
+        [FromQuery] int COMP_Codigo, [FromQuery] int Id_Trabajador, [FromQuery] string Tipo_trabajador, [FromQuery] string Apellido_Paterno, [FromQuery] string Apellido_Materno,
+        [FromQuery] string Nombres, [FromQuery] string Identificacion, [FromQuery] string Entidad_Bancaria, [FromQuery] string CarnetIESS, [FromQuery] string Direccion,
+        [FromQuery] string Telefono_Fijo, [FromQuery] string Telefono_Movil, [FromQuery] string Genero, [FromQuery] string Nro_Cuenta_Bancaria, [FromQuery] string Codigo_Categoria_Ocupacion,
+        [FromQuery] string Ocupacion, [FromQuery] string Centro_Costos, [FromQuery] string Nivel_Salarial, [FromQuery] string EstadoTrabajador, [FromQuery] string Tipo_Contrato,
+        [FromQuery] string Tipo_Cese, [FromQuery] string EstadoCivil, [FromQuery] string TipodeComision, [FromQuery] DateTime FechaNacimiento, [FromQuery] DateTime FechaIngreso,
+        [FromQuery] DateTime FechaCese, [FromQuery] int PeriododeVacaciones, [FromQuery] DateTime FechaReingreso, [FromQuery] DateTime Fecha_Ult_Actualizacion,
+        [FromQuery] string EsReingreso, [FromQuery] int BancoCTA_CTE, [FromQuery] string Tipo_Cuenta, [FromQuery] int RSV_Indem_Acumul, [FromQuery] int Año_Ult_Rsva_Indemni,
+        [FromQuery] int Mes_Ult_Rsva_Indemni, [FromQuery] int FormaCalculo13ro, [FromQuery] int FormaCalculo14ro, [FromQuery] int BoniComplementaria, [FromQuery] int BoniEspecial,
+        [FromQuery] int Remuneracion_Minima, [FromQuery] int CuotaCuentaCorriente, [FromQuery] string Fondo_Reserva)
+        {
+            var url = $"/api/Varios/TrabajadorUpdate?COMP_Codigo={COMP_Codigo}&Id_Trabajador={Id_Trabajador}&Tipo_trabajador={Tipo_trabajador}&Apellido_Paterno={Apellido_Paterno}&Apellido_Materno={Apellido_Materno}&Nombres={Nombres}&Identificacion={Identificacion}&Entidad_Bancaria={Entidad_Bancaria}&CarnetIESS={CarnetIESS}&Direccion={Direccion}&Telefono_Fijo={Telefono_Fijo}&Telefono_Movil={Telefono_Movil}&Genero={Genero}&Nro_Cuenta_Bancaria={Nro_Cuenta_Bancaria}&Codigo_Categoria_Ocupacion={Codigo_Categoria_Ocupacion}&Ocupacion={Ocupacion}&Centro_Costos={Centro_Costos}&Nivel_Salarial={Nivel_Salarial}&EstadoTrabajador={EstadoTrabajador}&Tipo_Contrato={Tipo_Contrato}&Tipo_Cese={Tipo_Cese}&EstadoCivil={EstadoCivil}&TipodeComision={TipodeComision}&FechaNacimiento={FechaNacimiento}&FechaIngreso={FechaIngreso}&FechaCese={FechaCese}&PeriododeVacaciones={PeriododeVacaciones}&FechaReingreso={FechaReingreso}&Fecha_Ult_Actualizacion={Fecha_Ult_Actualizacion}&EsReingreso={EsReingreso}&BancoCTA_CTE={BancoCTA_CTE}&Tipo_Cuenta={Tipo_Cuenta}&RSV_Indem_Acumul={RSV_Indem_Acumul}&Año_Ult_Rsva_Indemni={Año_Ult_Rsva_Indemni}&Mes_Ult_Rsva_Indemni={Mes_Ult_Rsva_Indemni}&FormaCalculo13ro={FormaCalculo13ro}&FormaCalculo14ro={FormaCalculo14ro}&BoniComplementaria={BoniComplementaria}&BoniEspecial={BoniEspecial}&Remuneracion_Minima={Remuneracion_Minima}&CuotaCuentaCorriente={CuotaCuentaCorriente}&Fondo_Reserva={Fondo_Reserva}";
+
+            var response = await _httpClient.PostAsync(url, null);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else
+            {
+                return BadRequest();
             }
         }
     }
